77e113ae3d6c871eb48f1372e2c033596d449019
feeli <feeli@tencent.com>
2020-03-25 10:18:57 +0800

    debug

--- src/main/java/cn/edu/bupt/Application.java
+++ src/main/java/cn/edu/bupt/Application.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.PropertySource;
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @ComponentScan({"cn.edu.bupt", "cn.edu.bupt.websocket"})
-@PropertySource({"classpath:disconf.properties"})
-@ImportResource({"classpath:disconf.xml"})//引入disconf
+//@PropertySource({"classpath:disconf.properties"})
+//@ImportResource({"classpath:disconf.xml"})//引入disconf
 public class Application {
     public static void main(String[] args) {
         ElasticsearchRestClient.initElasticsearchRestClient("device-access");

f8e1c33211fd1cc03377eb45bd4067e67e80d1f3
feeli <feeli@tencent.com>
2020-04-01 01:14:39 +0800

    debug JsonArray

--- src/main/java/cn/edu/bupt/dao/timeseries/CassandraBaseTimeseriesDao.java
+++ src/main/java/cn/edu/bupt/dao/timeseries/CassandraBaseTimeseriesDao.java
@@ -214,7 +214,7 @@ public class CassandraBaseTimeseriesDao extends CassandraAbstractAsyncDao implem
 
     private long toPartitionTs(long ts) {
         LocalDateTime time = LocalDateTime.ofInstant(Instant.ofEpochMilli(ts), ZoneOffset.UTC);
-        long res = tsFormat.truncatedTo(time).toInstant(ZoneOffset.UTC).toEpochMilli()
+        long res = tsFormat.truncatedTo(time).toInstant(ZoneOffset.UTC).toEpochMilli();
         logger.info("@@@@@@@@@@@@@@@@time = {}",res);
         return res ;
     }
